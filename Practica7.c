07c0a09d291800ef83a93bac407ef4ff26f41ec0
EdnaLeal <60495634+EdnaLeal@users.noreply.github.com>
2020-06-05 18:46:23 -0500

    Add files via upload

--- Practica7.c
+++ Practica7.c
@@ -1,22 +1,39 @@
-/*	Edna Leal, 1913447 */
+/* Edna Leal 1913447 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include<stdbool.h>
 
 int main()
 {
-	int base, multi, n, i;
+	int max, NumEvaluar, div, res, indice;
+	bool Numeros[100], EsDivisible;
 	
-	printf ("Ingresar un numero para utilizar de base \n");
-	scanf ("%d", &base);
-	printf ("Ingresar el numero de multiplos a generar \n");
-	scanf ("%d", &n);
-	printf ("Los multiplos gererados de la base %d son: \n", base);
-	for (i=0; i<n; i++)
+	printf("Numero maximo:\n");
+	scanf("%d", &max);
+	for(indice=0; indice<max; indice++)
 	{
-		multi = base * i;
-		printf ("%d \n", multi);
+		//Es primo
+		NumEvaluar = indice + 1;
+		res = 1;
+		EsDivisible = res == 0;
+		div=2;
+		
+		while(div<NumEvaluar && EsDivisible == false)
+		{
+			res = NumEvaluar % div;
+			//printf("p?:%d n:%d r:%d \n", NumEvaluar, div, res);
+			EsDivisible = res == 0;
+			div++;
+		}
+		//printf("Es primo? %d\n", EsDivisible);
+		Numeros[indice] =! EsDivisible;
 	}
-	system ("pause");
-	return 0;
+	
+	printf("\nResultado\n");
+	for(indice=0; indice<max; indice++)
+	{
+		NumEvaluar = indice + 1;
+		printf("Numero: %d es primo: %d\n", NumEvaluar, Numeros[indice]);
+	}
+
 }
